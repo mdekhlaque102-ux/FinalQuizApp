7cc4d1c67f323fd78e5529d648162b778e055907
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 17:19:03 +0530

    Create main-manifest.js

---
+++ data /main-manifest.js
@@ -0,0 +1,8 @@
+// File: /data/main-manifest.js
+// Yeh master file hai jo saare subjects ko ek jagah jodti hai.
+
+const quizDataBySubject = {
+  "science": scienceData // Data from _science-manifest.js
+  // Future mein naye subjects (jaise historyData) yahan add honge
+};
+
